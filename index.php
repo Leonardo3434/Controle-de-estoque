5b811918f2f2130eb56816db8acd7a7867988003
Leonardo3434 <48839602+Leonardo3434@users.noreply.github.com>
2019-03-22 15:40:05 -0300

    Iniciando Projeto
    Inicio de projeto....

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Sistema de Login</title>
+
+  <link real="stylesheet" type="text/css" href="CSS/style.css">
+
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="teste">
+  <form>
+  <div class="form-group">
+    <label Nome</label>
+    <input type="text" class="form-control" id="nome" placeholder="Nome" name="nome">
+  </div>
+  <div class="form-group">
+    <label Senha</label>
+    <input type="number" class="form-control" id="senha" placeholder="Senha">
+  </div>
+  
+</div>
+  <button type="submit" class="btn btn-default">Logar</button>
+</form>
